e76a7dcb052ea74f9414a230f3eba36de6c8772d
Shlomi Moreh <Shlomi295@gmail.com>
2022-08-27 15:14:02 +1000

    Added main page tests
    Basic verification of the navigation bar elements.

---
+++ specs/main.spec
@@ -0,0 +1,20 @@
+# Main webApp tests
+
+A test suite to confirm the web application works as expected
+
+This step to load the web application
+
+## Verify the navigation bar
+* Open Web application
+* Confirm items in navigation bar
+
+   |description|
+   |-----------|
+   |Home       |
+   |About      |
+   |FAQ        |
+   |Contact    |
+
+* Confirm image in navigation bar "/static/images/moplogo.png"
+* Confirm image in navigation bar "/static/City_Of_Melbourne_Logo.png"
+* Confirm button in navigation bar "https://github.com/D2I-Melbourne/MOP"
--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -17,9 +17,12 @@ const {
     text,
     into,
     textBox,
+    image,
+    button,
     evaluate
 } = require('taiko');
 const assert = require("assert");
+const { Console } = require('console');
 const headless = process.env.headless_chrome.toLowerCase() === 'true';
 
 beforeSuite(async () => {
@@ -66,11 +69,42 @@ step("Open todo application", async function () {
     await goto("todo.taiko.dev");
 });
 
+step("Open Web application", async function () {
+    await goto("https://mop-code-webapp-e5xbpzcnea-ts.a.run.app/");
+});
+
 step("Must not have <table>", async function (table) {
     for (var row of table.rows) {
         assert.ok(!await text(row.cells[0]).exists(0, 0));
     }
 });
+
+step("Confirm item in navigation bar <message>", async function (message) {
+        assert.ok(await text(message.toString()).exists(0, 0));
+});
+
+step("Confirm items in navigation bar <table>", async function (table) {
+    for (var row of table.rows) {
+        console.log("Verifing element " + row.cells[0])
+        assert.ok(await text(row.cells[0]).exists(0, 0));
+    }
+});
+
+step("Confirm image in navigation bar <message>", async function (message) {
+    console.log("Verifing image " + message)
+        assert.ok(await image({src:message}).exists(0, 0));
+});
+
+step("Confirm button in navigation bar <message>", async function (message) {
+    console.log("Verifing button " + message)
+        assert.ok(await link({href:message}).exists(0, 0));
+});
+
+step("Confirm search bar"), async function(){
+    await evaluate($("/html/body/div[3]/section[2]/div[1]/div/input"), (element, args) =>{
+        element.querySelector(args[0]).innerText = 'Search use cases...';
+    })
+}
 
 step("Must display <message>", async function (message) {
     assert.ok(await text(message).exists(0, 0));
